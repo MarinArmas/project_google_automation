316f8bb26fcd8d4b829f5b3206c8ae67b87f99f2
Marin Armas <marin.hoiixx@hotmail.com>
2024-08-31 18:02:31 +0300

    process the txt files and send data to the API

---
+++ run.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+import os
+import requests
+
+def process_file(file_path, image):
+    fruits_dict = {}
+    try:
+        with open(file_path, 'r') as f:
+            lines = f.readlines()
+
+            # Ensure that there are enough lines in the file
+            if len(lines) < 4:
+                print(f"Skipping file {file_path}: Not enough data")
+                return None
+
+            fruits_dict['name'] = lines[0].strip()
+            fruits_dict['weight'] = lines[1].strip()
+            fruits_dict['description'] = lines[2].strip()
+            fruits_dict['image_name'] = image
+        
+        return fruits_dict
+
+    except (IOError, IndexError, SyntaxError) as e:
+        print(f"Error processing file {file_path}: {e}")
+        return None
+
+def send_post_request(fruits_dict):
+    try:
+        # Change the URL
+        response = requests.post('http://104.197.242.79/feedback', json=fruits_dict)
+        
+        if response.status_code == 201:
+            print("Successfully posted")
+        else:
+            print("Failed to post:", response.status_code)
+    except requests.RequestException as e:
+        print(f"Request failed: {e}")
+
+def main():
+    # Change the directory
+    directory = './data'
+    for filename in os.listdir(directory):
+        file_path = os.path.join(directory, filename)
+        image = os.path.splitext(filename)[0] + ".jpeg"
+        if os.path.isfile(file_path):
+            feedback_dict = process_file(file_path, image)
+            if feedback_dict:  # Check if processing was successful
+                send_post_request(feedback_dict)
+
+if __name__ == "__main__":
+    main()
